37edefa3f0245d8cc9402db2c070d8323d49f2f7
norsker <emilgraaenorsker@gmail.com>
2025-10-15 11:02:29 +0200

    use a package to run this through git

--- flake.nix
+++ flake.nix
@@ -32,10 +32,16 @@
         '';
       };
 
+      packages.${system}.default = pkgs.writeShellScriptBin "obs-ndi" ''
+        export NDI_IP=127.0.0.1
+        exec ${pkgs.wrapOBS { plugins = with pkgs.obs-studio-plugins; [ obs-ndi ]; }}/bin/obs --start-virtual-cam "$@"
+      '';
+
     };
 }
-    # needs to be set in configuration.nix
-    # avahi = {
+    # needs to be set in configuration.nix:
+    # 
+    # services.avahi = {
     #   enable = true;
     #   nssmdns = true;
     #   openFirewall = true;
@@ -44,5 +50,23 @@
     #     userServices = true;
     #     addresses = true;
     #     workstation = true;
+    #   };
+    # };
+    # 
+    # # Optional: Create dedicated user for OBS service
+    # users.users.obs = {
+    #   isNormalUser = true;
+    #   extraGroups = [ "audio" "video" "render" ];
+    # };
+    # 
+    # # Example of how to define systemd services directly in configuration.nix:
+    # systemd.services.myservice = {
+    #   enable = true;
+    #   description = "My Service";
+    #   wantedBy = [ "graphical.target" ];
+    #   path = [ pkgs.nix ];
+    #   serviceConfig = {
+    #     ExecStart = "nix run git+https://emilnorsker/ai-know-you-afvikling.git";
+    #     User = "obs";
     #   };
     # };
