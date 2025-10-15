5169a0c9d020712420be3db9867652b3e933acdd
norsker <emilgraaenorsker@gmail.com>
2025-10-15 10:06:32 +0200

    add install

--- default.nix
+++ default.nix
@@ -12,9 +12,12 @@
   ];
 
   systemd.user.services.obs-autostart = {
-    Unit.Description = "OBS Studio autostart";
-    Service.ExecStart = "${pkgs.wrapOBS { plugins = with pkgs.obs-studio-plugins; [ obs-ndi ]; }}/bin/obs --minimize-to-tray --start-virtual-cam";
-    Service.Environment = [ "NDI_IP=127.0.0.1" ];
-    Install.WantedBy = [ "default.target" ];
+    enable = true;
+    description = "OBS Studio autostart";
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.wrapOBS { plugins = with pkgs.obs-studio-plugins; [ obs-ndi ]; }}/bin/obs --minimize-to-tray --start-virtual-cam";
+      Environment = [ "NDI_IP=127.0.0.1" ];
+    };
   };
 }
